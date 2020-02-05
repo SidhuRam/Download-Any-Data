5941347f93ae676752a099112d6e13cd1712cc47
Sidhu Ram <sidhu.lsk@gmail.com>
2020-02-05 11:59:21 +0530

    Update Download Any Data.py

--- Download Any Data.py
+++ Download Any Data.py
@@ -1,27 +1,50 @@
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
-
 #Alphavantage Codes##
 #Nifty50 = ^NSEI
 #BankNifty = ^NSEBANK
-api_key = 'QWYIM8PV23DY5BE9';
-scripname = "^NSEI"
-#EOD DATA
-ts = TimeSeries(key = api_key,output_format = 'pandas', indexing_type='integer');
-#fulldata = ts.get_daily(symbol="NSE:"+scripname, outputsize = 'full');
 
-def eoddata():
-    eoddata = ts.get_daily(symbol="NSE:"+scripname, outputsize = 'full');
-    fdf = pd.DataFrame(eoddata[0]);
-    fdf.dropna(inplace = True);
-    fdf = fdf[fdf['5. volume']>1];
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+
+api_key = 'QWYIM8PV23DY5BE9';
+
+### INITIALIZE THE STOCK NAME HERE -- ####
+scripname = "^NSEI"
+##########################################
+
+#Available intraday timeframes = 1min, 5min,15min, 30min, 60min
+### INTIATIZE INTRADAY TIMEFRAME HERE ---- ###
+intratimeframe = '30min'
+
+if scripname == "^NSEI":
+    symbol = scripname
+elif scripname == "^NSEBANK":
+    symbol = scripname
+else:
+   symbol = "NSE:"+scripname
+    
+ts = TimeSeries(key = api_key,output_format = 'pandas', indexing_type='integer');
+
+#EOD DATA
+def eoddata(symbol):
+    eoddata = ts.get_daily(symbol=symbol, outputsize = 'full');
+    eoddf = pd.DataFrame(eoddata[0]);
+    eoddf.dropna(inplace = True);
+    eoddf = eoddf[eoddf['5. volume']>1];
     #fdf.to_csv('PUT THE PATH HERE',sep = ',');
-    fdf.to_csv('D:\\Python Exercices\\',sep = ',');
+    eoddf.to_csv('D:\\Python Exercices\\DataFiles\\EODData.csv',sep = ',');
 
 #INTRADAY DATA
-def intraday():
-    intradaydata = ts.get_intraday(symbol="NSE:"+scripname, outputsize = 'full');
+def intraday(symbol,intratimeframe):
+    intradaydata = ts.get_intraday(symbol=symbol,interval = intratimeframe,outputsize = 'full');
     fdf = pd.DataFrame(intradaydata[0]);
     fdf.dropna(inplace = True);
     fdf = fdf[fdf['5. volume']>1];
-    fdf.to_csv('D:\\Python Exercices\\',sep = ',');
+    fdf.to_csv('D:\\Python Exercices\\DataFiles\\IntradayData.csv',sep = ',');
+
+
+
+#Call this function for EOD DATA
+eoddata(symbol);
+################################
+#Call this function for Intraday Data:
+intraday(symbol,intratimeframe);
